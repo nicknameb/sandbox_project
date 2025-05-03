0f7022ddd56cc795257eefe6e4d5c83f53a54ac7
nicknameb <85395400+nicknameb@users.noreply.github.com>
2025-05-04 00:20:28 +0300

    Update sandbox_logic.cpp

--- sandbox_logic.cpp
+++ sandbox_logic.cpp
@@ -1,23 +1,53 @@
 #include "sandbox_logic.h"
 #include <iostream>
 #include <windows.h>
-
+#include "network_watcher.h" 
+#include "scanner.h" 
+#include <Psapi.h> 
+#include "pe_analyzer.h"
 using namespace std; 
 
-void Sandbox::executeInSandbox(const string& filename) {
+void Sandbox::executeInSandbox(const string& filename) { 
+    NetworkWatcher networkWatcher; 
+    Scanner scanner; 
+    PEAnalyzer peanalyzer;
+
     cout << "Running " << filename << " in sandbox..." << endl;
 
     STARTUPINFOA si = { sizeof(si) };
     PROCESS_INFORMATION pi;
 
     if (CreateProcessA(NULL, const_cast<char*>(filename.c_str()), NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi)) {
+
         cout << "Suspended Process Created! PID: " << pi.dwProcessId << endl;
-        ResumeThread(pi.hThread);
-        WaitForSingleObject(pi.hProcess, INFINITE);
+
+
+        char name_buffer[MAX_PATH];
+        GetModuleFileNameExA(pi.hProcess, NULL, (LPSTR)name_buffer, MAX_PATH);
+        bool sus_api = scanner.scanSuspendedProcess(pi.hProcess);
+        cout << "found suspicious api: " << sus_api << " in process: "<< name_buffer << endl;
+
+        bool suspicous_imprts = peanalyzer.scanImports(filename); 
+        
+
+        if (sus_api || suspicous_imprts)
+        { 
+            cout << "malicious signature detected, did not run file" << endl; 
+            return;
+        } 
+
+        ResumeThread(pi.hThread); 
+        
+
+        WaitForSingleObject(pi.hProcess, INFINITE); 
+
+
         CloseHandle(pi.hProcess);
         CloseHandle(pi.hThread);
     }
     else {
-        cerr << "Failed to run file in sandbox!" << endl;
+        cerr << "Failed to run file in sandbox!" << endl; 
+        cout << GetLastError() << endl;
     }
 }
+
