9fada5c8c9abe9742b863ae571dfd07072e0fa64
nicknameb <85395400+nicknameb@users.noreply.github.com>
2025-05-13 17:31:13 +0300

    updated to include vm

--- main.cpp
+++ main.cpp
@@ -4,30 +4,43 @@
 #include "monitor.h"
 #include "network_watcher.h"
 #include "sandbox_logic.h"
-#include <iostream>
-#include "pe_analyzer.h"
+#include "pe_analyzer.h" 
+#include <windows.h> 
+#include "sandbox_vm.h"
 using namespace std; 
 
-int main() {
+int main() { 
+    
+
+
+
+    const string vboxPath = "C:\\Program Files\\Oracle\\VirtualBox\\VBoxManage.exe"; 
+    const string vmName = "SandBoxVM"; 
+    const string current_snapshot = "Snapshot 30";  
+    const string guestfile_path = "C:\\Users\\jacob\\Downloads\\malware_folder\\"; 
+
+    Sandbox_vm sandbox_vm;
     Scanner scanner;
     Monitor monitor; 
+    PEAnalyzer peanalyzer;
     Sandbox sandbox; 
-    string fileToScan = "C:\\Users\\jacob\\source\\repos\\malwarec\\x64\\Debug\\malwarec.exe"; // Replace with actual path
+    string fileToScan = "C:\\Users\\jacob\\source\\repos\\registry_test2\\x64\\Debug\\registry_test2.exe"; // Replace with actual path
 
     cout << "[1] Scanning file..." << endl;
-    bool isMalicious = scanner.scanFile(fileToScan, "signatures.txt"); 
-    bool isMalicious_unencrypted = scanner.scanFile(fileToScan, "unencrypted_signatures.txt"); 
+    bool hashsignature_found = scanner.scanFile(fileToScan, "signatures.txt"); 
+    bool unencrypted_signature_found = scanner.scanFile(fileToScan, "unencrypted_signatures.txt"); 
 
-    cout << "malicious signature found: (0/1) " << isMalicious << endl;
-    cout << "malicious unencrypted signature found: (0/1) " << isMalicious_unencrypted << endl;
+    cout << "unencrypted signature found: (0/1)" << unencrypted_signature_found << endl;
 
-    
-    if (isMalicious || isMalicious) {
+    bool sus_imports = peanalyzer.scanImports(fileToScan);
+    cout <<"containts suspicious improts: (0/1) "<< sus_imports << endl; 
+
+    if (true) {
         cout << "[2] Running in sandbox for further analysis..." << endl;
         sandbox.executeInSandbox(fileToScan);
-    }
+    }   
 
-
+    sandbox_vm.RunVirtualBoxVM(vboxPath, vmName, current_snapshot, fileToScan, guestfile_path);
 
     return 0;
 }
