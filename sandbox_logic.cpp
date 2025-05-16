f58b7e44287c3bb068c98b473b79ab54ec693b05
nicknameb <85395400+nicknameb@users.noreply.github.com>
2025-05-16 20:44:21 +0300

    Update sandbox_logic.cpp

--- sandbox_logic.cpp
+++ sandbox_logic.cpp
@@ -1,41 +1,47 @@
 #include "sandbox_logic.h"
 #include <iostream>
 #include <windows.h>
-#include "network_watcher.h" 
 #include "scanner.h" 
 #include <Psapi.h> 
-#include "pe_analyzer.h"
+#include "pe_analyzer.h" 
+#include <fstream> 
 using namespace std;  
 
 
 
-void Sandbox::executeInSandbox(const string& filename) { 
-    NetworkWatcher networkWatcher; 
+void ScanProcess::ScanCreatedProcess(const string& filename) {  
     Scanner scanner; 
+    char path_buffer[MAX_PATH];
+    GetCurrentDirectoryA(MAX_PATH, path_buffer);
+    string current_dir(path_buffer);
+    string log_file = current_dir + "\\scan_output.txt";
 
-    cout << "Running " << filename << " in sandbox..." << endl;
+    ofstream log(log_file, ios::app);
+
+    log << "Running " << filename << " in " << endl;
 
     STARTUPINFOA si = { sizeof(si) };
     PROCESS_INFORMATION pi;
 
     if (CreateProcessA(NULL, const_cast<char*>(filename.c_str()), NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi)) {
 
-        cout << "Suspended Process Created! PID: " << pi.dwProcessId << endl;
+        log << "Suspended Process Created! PID: " << pi.dwProcessId << endl;
 
 
         char name_buffer[MAX_PATH];
         GetModuleFileNameExA(pi.hProcess, NULL, (LPSTR)name_buffer, MAX_PATH);
-        bool sus_api = scanner.scanSuspendedProcess(pi.hProcess);
-        cout << "found suspicious api in suspended process (0/1): " << sus_api << " in process: "<< name_buffer << endl;
+        bool sus_api = scanner.scanSuspendedProcess(pi.hProcess);  
 
+        if (sus_api)
+        {
+            log << "SUSPICIOUS_API FOUND IN SUSPENDED PROCESS: " << name_buffer << endl;
+        }
 
         CloseHandle(pi.hProcess);
         CloseHandle(pi.hThread);
     }
     else {
-        cerr << "Failed to create process!" << endl; 
-        cout << GetLastError() << endl;
+        log << "ERROR: Failed to create process!" << endl; 
+        log << GetLastError() << endl;
     }
 }
-
-
